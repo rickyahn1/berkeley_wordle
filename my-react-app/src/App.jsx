02a0e41da955ee1de80a7bb51efa4c6bbfd71b27
Ricky Ahn <rickyahn1@berkeley.edu>
2023-11-14 18:48:06 -0800

    basic home page functionality in App.jsx and basic functionality in Home

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import React from 'react';
+import Home from './pages/Home';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState('home');
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Wordle</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      {page === 'home' && <Home onPageChange={handlePageChange} />}
+      {/* {page === 'cs161' && <CS161 onPageChange={handlePageChange} />}
+      {page === 'cs162' && <CS162 onPageChange={handlePageChange} />} */}
+    </main>
   )
 }
 
--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import "../styles/Home.css"
 function Home() {
     return (
         <div>
+            "Hello"
         </div>
     )
 }
