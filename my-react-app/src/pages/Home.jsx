16466eb8b2f847dd32041a905b8902f9240cd304
Minoo Kim <minookim@berkeley.edu>
2023-11-14 18:53:09 -0800

    figma code in Home.jsx

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Component } from "./Component";
+import { Group } from "./Group";
+import { Leaderboard } from "./Leaderboard";
+import { PersonalProgress } from "./PersonalProgress";
 import "../styles/Home.css"
 
 function Home() {
     return (
-        <div>
-            "Hello"
+        <div className="home">
+            <div className="div-2">
+                <Group className="group-2" />
+                <Component className="component-1" />
+                <div className="overlap">
+                    <div className="text-wrapper-3">Select Category</div>
+                        <img className="drop-down-arrow" alt="Drop down arrow" src="drop-down-arrow-1.png" />
+                    </div>
+                <div className="overlap-group">
+                    <div className="text-wrapper-4">Login</div>
+                    <div className="text-wrapper-5">Alerts</div>
+                    <img className="user" alt="User" src="user-1.png" />
+                </div>
+                <Leaderboard className="leaderboard-instance" />
+                <PersonalProgress className="personal-progress-instance" />
+                <img className="bell" alt="Bell" src="bell-1.png" />
+            </div>
         </div>
     )
 }
