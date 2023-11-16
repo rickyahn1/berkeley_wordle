f9f699c986403cf6070ff5cd52af3608c250f863
Minoo Kim <minookim@berkeley.edu>
2023-11-15 18:00:04 -0800

    updates

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { Component } from "./Component";
-import { Group } from "./Group";
-import { Leaderboard } from "./Leaderboard";
-import { PersonalProgress } from "./PersonalProgress";
-import "../styles/Home.css"
+import styled from 'styled-components';
 
 const HomeContainer = styled.div`
-  background-color: #ffffff;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 100%;
 `;
 
 const Title = styled.h2`
